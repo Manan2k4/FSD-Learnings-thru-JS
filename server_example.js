65a77d7d027afe3b6f2489e2cadfc64d8e28d708
Manan2k4 <ghoniyamanan@gmail.com>
2025-07-08 10:25:06 +0530

    Update server_example.js

--- server_example.js
+++ server_example.js
@@ -1,8 +1,19 @@
 var http = require('http')
+var a = 10
+var b = 20
+var c = a+b
+var msg = ''
+if(c == 30) {
+    msg = 'C value = 30'
+}
+else {
+    msg = 'C value != 30'
+}
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-type': 'text/html'})
-    res.write("Hello World ")
-    res.write("<b> Use of B tag </b>")
-    res.write("Bye")
+    res.write("<b>Hello World </b>")
+    res.write(`<br/>A value: ${a} <br/> B value: ${b} <br/> Sum is ${a + b} <br/>`)
+    res.write(msg)
+    res.write("<br/>Bye")
 }).listen(3000)
 console.log("Server started http://127.0.0.1:3000")
