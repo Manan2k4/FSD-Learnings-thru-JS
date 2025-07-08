579f95e2df2b94d6c56f6c24ae3818b2fbb3417e
Manan2k4 <ghoniyamanan@gmail.com>
2025-07-08 10:34:59 +0530

    Create server_ex2.js

---
+++ server_ex2.js
@@ -0,0 +1,14 @@
+var http = require('http')
+
+http.createServer((req, res) => {
+    if(req.url == '/home'){
+        res.end("<h1> Home Page </h1>")
+    }
+    else if(req.url == '/about'){
+        res.end("<h1> About Page </h1>")
+    }
+    else {
+        res.end("<h1> Welcome Page </h1>")
+    }
+}).listen(4000)
+console.log("Server started @ http://127.0.0.1:4000")
