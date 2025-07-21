4ba4ad89ab58b845f98296bf86d4bbfea538097a
Manan2k4 <ghoniyamanan@gmail.com>
2025-07-21 13:35:08 +0530

    some implementation
    action button not active on frontend

--- Prac3/Prac-App/src/App.jsx
+++ Prac3/Prac-App/src/App.jsx
@@ -3,11 +3,17 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Demo1 from './components/demo1'
+import Demo2 from './components/demo2'
 
 function App() {
   return (
     <>
       <Demo1 dept="CSE" institute="CSPIT"/>
+      <Demo1 dept="CE" institute="CSPIT"/>
+      <Demo1>
+        <button>Action</button>
+      </Demo1>
+      <Demo2 dept='CSE'/>
     </>
   )
 }
--- Prac3/Prac-App/src/components/demo2.jsx
+++ Prac3/Prac-App/src/components/demo2.jsx
@@ -1,4 +1,11 @@
 import { Component } from "react";
+
 class Demo2 extends Component {
-    
+    render(){
+        return(
+            <h1>Welcome,to Charusat-CSPIT</h1>
+        )
+    }
 }
+
+export default Demo2
