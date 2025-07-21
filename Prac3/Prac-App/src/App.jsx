1843a526c07e9afd39911ab8aa12b996e334e0ee
Manan2k4 <ghoniyamanan@gmail.com>
2025-07-21 12:43:50 +0530

    Creating Prac3 App

---
+++ Prac3/Prac-App/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Demo1 from './components/demo1'
+
+function App() {
+  return (
+    <>
+      <Demo1/>
+    </>
+  )
+}
+
+export default App
---
+++ Prac3/Prac-App/src/components/demo1.jsx
@@ -0,0 +1,8 @@
+// function Demo1() {
+//     return <h1>Welcome!!!</h1>
+// }
+
+const Demo1 = () => {
+    <h1>Welcome</h1>
+}
+export default Demo1
