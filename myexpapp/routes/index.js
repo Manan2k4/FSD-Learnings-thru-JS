2815106636dd218f7a8a7b6f27005da2354e09af
Manan2k4 <ghoniyamanan@gmail.com>
2025-08-05 09:45:34 +0530

    minor changes myexpapp

--- myexpapp/routes/index.js
+++ myexpapp/routes/index.js
@@ -6,10 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+  res.render('home');
 });
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Express' });
+  res.render('about');
 });
 
 module.exports = router;
